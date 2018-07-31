c3f7e2d748c27700feec979349bacf963100dad9
Sandeep <sdpnis7@gmail.com>
2018-08-01 01:02:21 +0530

    GUI based login

--- login.py
+++ login.py
@@ -1,13 +1,11 @@
 import mechanize
-user=raw_input("Enter username")
-print user
-pas=raw_input("Enter password\n")
-br=mechanize.Browser()
-br.set_handle_robots(False)
-br.add_header=[('User','Firefox')]
-res=br.open("http://172.16.32.1:8090")
-br.select_form(nr=0)
-br.form['username']=user
-br.form['password']=pas
-res=br.submit()
-print res
+def login(user,pas):	
+	br=mechanize.Browser()
+	br.set_handle_robots(False)
+	br.add_header=[('User','Firefox')]
+	res=br.open("http://172.16.32.1:8090")
+	br.select_form(nr=0)
+	br.form['username']=user
+	br.form['password']=pas
+	res=br.submit()
+	print res

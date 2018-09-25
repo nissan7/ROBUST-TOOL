277469cde2a79cc723c285fceb5b60c3ca58787c
Sandeep <sdpnis7@gmail.com>
2018-09-25 14:51:31 +0530

    FILE SHARING TAB ADDED AND BUG FIXED

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ def login(user,pas):
 	try:	
 		res=br.open("http://172.16.32.1:8090")
 	except: 
-		return 2
+		return 2 #wifi is not connected to COLLEGE NETWORK
 	br.select_form(nr=0)
 	br.form['username']=user
 	br.form['password']=pas
@@ -16,9 +16,14 @@ def login(user,pas):
 	soup=BeautifulSoup(html,"html5lib")
 	item=soup.find('message')
 	msg=item.string
+	#print msg
 	if msg[7]=='T':
 		return 0
 	elif msg[10]==' ':
 		return 1
 	else:
 		return 2
+#1-logged in
+#0-incorrect
+#3-data limit
+#2-connect to nit wifi
--- maingui.py
+++ maingui.py
@@ -14,12 +14,12 @@ readed=fil.read()
 window=Tk()
 
 def lclicked(event):
-	clabel=Label(window,text="logged in",width=25,height=3)
-	ilabel=Label(window,text="Well! that didn't work",width=25,height=3)
-	dlabel=Label(window,text="Data limit reached!",width=25,height=3)
-	slabel=Label(window,text="Connect to NIT Sikkim network\nSERVER DID NOT RESPOND",width=25,height=3)
-	ulabel=Label(window,text="please provide user ID",width=25,height=3)
-	plabel=Label(window,text="please provide password",width=25,height=3)
+	clabel=Label(login_f,text="logged in",width=25,height=3)
+	ilabel=Label(login_f,text="Well! that didn't work",width=25,height=3)
+	dlabel=Label(login_f,text="Data limit reached!",width=25,height=3)
+	slabel=Label(login_f,text="Connect to NIT Sikkim network\nSERVER DID NOT RESPOND",width=25,height=3)
+	ulabel=Label(login_f,text="please provide user ID",width=25,height=3)
+	plabel=Label(login_f,text="please provide password",width=25,height=3)
 	userid=user.get()
 	pasid=pas.get()
 	
@@ -32,7 +32,10 @@ def lclicked(event):
 	
 	
 		
-	stat=l.login(userid,pasid)
+	stat=l.login(userid,pasid)   	#1-logged in
+					#0-incorrect
+					#3-data limit
+					#2-connect to nit wifi
 	result="no"	
 	if stat==1:
 		global logged
@@ -46,7 +49,7 @@ def lclicked(event):
 		dlabel.place(x=msgx,y=msgy)
 	global readed
 	userinfo=readed.split('\n')
-	if stat==1 and (userinfo[0]!=user.get()):
+	if stat==1 and (userinfo[0]!=user.get() or userinfo[1]!=pas.get()):
 		result = tkMessageBox.askquestion("FAST LOGIN", "Save this info for fast login ?\nprevious login info will be erased !", icon='info')
 		
 	if result=="yes" and stat==1:
@@ -63,11 +66,11 @@ def lclicked(event):
 	
 
 def fast_clicked(event):
-	clabel=Label(window,text="fast logged in",width=25,height=3)
-	
-	flabel=Label(window,text="user info not added for\n Fast login \nlogin one time to save",width=25,height=3)
-	slabel=Label(window,text="Connect to NIT Sikkim network\nSERVER DID NOT RESPOND",width=25,height=3)
-	dlabel=Label(window,text="Data limit reached!",width=25,height=3)	
+	clabel=Label(login_f,text="fast logged in",width=25,height=3)
+	ilabel=Label(login_f,text="Well! that didn't work\npassword is changed",width=25,height=3)
+	flabel=Label(login_f,text="user info not added for\n Fast login \nlogin one time to save",width=25,height=3)
+	slabel=Label(login_f,text="Connect to NIT Sikkim network\nSERVER DID NOT RESPOND",width=25,height=3)
+	dlabel=Label(login_f,text="Data limit reached!",width=25,height=3)	
 	if len(readed)==0:
 		
 		flabel.place(x=msgx,y=msgy)
@@ -79,43 +82,68 @@ def fast_clicked(event):
 			global logged
 			logged=1
 			clabel.place(x=msgx,y=msgy)
+		elif stat==0:
+			ilabel.place(x=msgx,y=msgy)
 		elif stat==2:
 			slabel.place(x=msgx,y=msgy)
 		else:
 			dlabel.place(x=msgx,y=msgy)
 
 def logout_clicked(event):
-	logoutlabel=Label(window,text="LOGGED OUT",width=25,height=3)
-	lflabel=Label(window,text="NOT LOGGED IN!",width=25,height=3)
+	logoutlabel=Label(login_f,text="LOGGED OUT",width=25,height=3)
+	lflabel=Label(login_f,text="NOT LOGGED IN!",width=25,height=3)
+	#dummyframe.grid(row=0,column=0,padx=2,pady=2,sticky=NW)
+	#login_f.grid_forget()
+
 	if logged==0:
 		lflabel.place(x=msgx,y=msgy)
 	else:
 		logoutlabel.place(x=msgx,y=msgy)
 
+def toggle_1(event):
+	file_f.grid_forget()
+	login_f.grid(row=1,column=0,padx=2,pady=2,sticky=NW)
+	
+	
+def toggle_2(event):
+	login_f.grid_forget()
+	file_f.grid(row=1,column=0,padx=2,pady=2,sticky=NW)
 
+	
 
-add_img.app_logo(window)
+#add_img.app_logo(window)
 window.title("ROBUST-TOOL")
-window.geometry("400x300")
+window.geometry("410x320")
 window.resizable(10,10)
-mainframe=Frame(window,height=296,width=396,bg="#424c5e")
-mainframe.grid(row=0,column=0,padx=2,pady=2,sticky=NW)
+nav_f=Frame(window,height=30,width=410,bg="#424c5e")
+nav_f.grid(row=0,column=0)
+toggle1=Button(nav_f,text="LOGIN")
+toggle2=Button(nav_f,text="FILE_SHARING")
+file_f=Frame(window,height=286,width=406,bg="#424c5e")
+login_f=Frame(window,height=286,width=406,bg="#424c5e")
+toggle1.bind('<Button-1>',toggle_1)
+toggle2.bind('<Button-1>',toggle_2)
+toggle1.place(x=2,y=2)
+toggle2.place(x=73,y=2)
 
-userl=Label(mainframe,text="USERNAME:")
-user=Entry(mainframe)
-pasl=Label(mainframe,text="PASSWORD:")
-pas=Entry(mainframe,show="*")
+login_f.grid(row=1,column=0,padx=2,pady=2,sticky=NW)
+file_f.grid(row=1,column=0,padx=2,pady=2,sticky=NW)
 
-fast_login=Button(mainframe,text="Fast Login")
+userl=Label(login_f,text="USERNAME:")
+user=Entry(login_f)
+pasl=Label(login_f,text="PASSWORD:")
+pas=Entry(login_f,show="*")
+
+fast_login=Button(login_f,text="Fast Login")
 fast_login.bind('<Button-1>',fast_clicked)
 
-logout=Button(mainframe,text="LOG-OUT")
+logout=Button(login_f,text="LOG-OUT")
 logout.bind('<Button-1>',logout_clicked)
 
-login=Button(mainframe,text="login")
+login=Button(login_f,text="login")
 login.bind('<Button-1>',lclicked)
 
-wlabel=Label(window,text="Welcome!",width=25,height=3)
+wlabel=Label(login_f,text="Welcome!",width=25,height=3)
 wlabel.place(x=msgx,y=msgy)
 
 userl.place(x=70,y=30)
